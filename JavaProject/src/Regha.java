ea1c23f349ebe0fba19097d20f6f6463d65086e0
PrasannaWade <PrasannaWade@VTLP89>
2022-11-18 16:23:23 +0530

    2nd commit

--- JavaProject/src/Regha.java
+++ JavaProject/src/Regha.java
@@ -4,6 +4,7 @@ public class Regha {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My first project");
+System.out.println("My 2nd project");
 	}
 
 }
